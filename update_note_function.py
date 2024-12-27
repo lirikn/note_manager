8cbe8f289fdb7038e6962ae22fb043dca191d187
kirill <k.nelyubov@gmail.com>
2024-12-28 00:40:24 +0300

    #25/12

---
+++ update_note_function.py
@@ -0,0 +1,44 @@
+# Позволяет пользователю вносить изменения в уже созданные заметки, сохраняя их актуальность.
+from datetime import datetime
+
+# Функция позволяет вводить несколько полей для изменения
+def input_check():
+    keys = ('username', 'title', 'content', 'status', 'issue_date')
+    input_keys = input(f"Какие данные вы хотите обновить? ({', '.join(keys)}): ").split(', ')
+    for key in input_keys:
+        if key not in keys:
+            print(f'некорректное имя поля {key}')
+            return
+    return input_keys
+
+def date_check(date):
+    try:
+        datetime.strptime(date, "%d-%m-%Y")
+    except:
+        return False
+    return True
+
+def update_note(note_):
+    print('Текущие данные заметки:')
+    print(note_)
+    input_keys = input_check()
+    while input_keys is None:
+        input_keys = input_check()
+    for key in input_keys:
+        new = input(f'Введите новое значение для {key}: ')
+        if new != '':
+            if key == 'issue_date':
+                while not date_check(new):
+                    new = input(f'неправильный формат даты {new}, введите заново дд-мм-гггг: ')
+            note_[key] = new
+    print('Заметка обновлена:')
+    print(note_)
+    return note_
+
+note = {'username': 'Алексей',
+        'title': 'Список покупок',
+        'content': 'Купить продукты на неделю',
+        'status': 'новая',
+        'created_date': '27-11-2024',
+        'issue_date': '30-11-2024'}
+note = update_note(note)
